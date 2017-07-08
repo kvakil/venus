045509e7c8e455e5343d7867f6811946ab60fe59
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-08 15:08:20 -0700

    Add ADD tests

---
+++ src/test/kotlin/simulator/impl/ADDTest.kt
@@ -0,0 +1,32 @@
+package venus.simulator.impl
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import venus.riscv.Instruction
+import venus.riscv.InstructionField
+import venus.simulator.SimulatorState
+import venus.simulator.DispatchTest
+import venus.simulator.FieldTest
+import venus.simulator.InstructionImplementation
+
+class ADDTest {
+    @Test
+    fun basicADD() {
+        val inst = Instruction(0b00000000000100010000000110110011)
+        val state = SimulatorState()
+        state.setReg(1, 10)
+        state.setReg(2, 20)
+        ADD.implementation(inst, state)
+        assertEquals(30, state.getReg(3))
+    }
+
+    @Test
+    fun overflowADD() {
+        val inst = Instruction(0b00000000000100010000000110110011)
+        val state = SimulatorState()
+        state.setReg(1, 2000000000)
+        state.setReg(2, 2000000000)
+        ADD.implementation(inst, state)
+        assertEquals(-294967296, state.getReg(3))
+    }
+}
