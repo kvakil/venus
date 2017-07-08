276c67036d591d69de0702a6c2a03f078d1b5d66
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-08 14:48:52 -0700

    Prevent writing to x0

--- src/main/kotlin/simulator/SimulatorState.kt
+++ src/main/kotlin/simulator/SimulatorState.kt
@@ -4,5 +4,5 @@ class SimulatorState {
     private val regs = IntArray(32)
     var pc: Int = 0
     fun getReg(i: Int) = regs[i]
-    fun setReg(i: Int, v: Int) { regs[i] = v }
+    fun setReg(i: Int, v: Int) { if (i != 0) regs[i] = v }
 }
---
+++ src/test/kotlin/simulator/SimulatorStateTest.kt
@@ -0,0 +1,22 @@
+package venus.simulator
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SimulatorStateTest {
+    @Test
+    fun storeLoadRegister() {
+        val state = SimulatorState()
+        state.setReg(1, 10)
+        assertEquals(10, state.getReg(1))
+        state.setReg(1, -10)
+        assertEquals(-10, state.getReg(1))
+    }
+
+    @Test
+    fun nowriteZeroRegister() {
+        val state = SimulatorState()
+        state.setReg(0, 10)
+        assertEquals(0, state.getReg(0))
+    }
+}
