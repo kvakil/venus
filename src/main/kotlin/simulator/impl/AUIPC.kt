9fd431165b49f553383523cdba68285d55b94cff
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-08 22:05:41 -0700

    Add AUIPC and LUI instructions

---
+++ src/main/kotlin/simulator/impl/AUIPC.kt
@@ -0,0 +1,23 @@
+package venus.simulator.impl
+
+import venus.riscv.Instruction
+import venus.riscv.InstructionField
+import venus.simulator.SimulatorState
+import venus.simulator.DispatchTest
+import venus.simulator.FieldTest
+import venus.simulator.InstructionImplementation
+
+object AUIPC {
+    val implementation = object : InstructionImplementation {
+        override operator fun invoke(inst: Instruction, state: SimulatorState) {
+            val rd: Int = inst.getField(InstructionField.RD)
+            val imm: Int = inst.getField(InstructionField.IMM_31_12) shl 12
+            state.setReg(rd, state.pc + imm)
+            state.pc += inst.length
+        }
+    }
+
+    val tests: List<DispatchTest> = listOf(
+        FieldTest(InstructionField.OPCODE, 0b0010111)
+    )
+}
