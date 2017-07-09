2c838ac3d5e51f74d5df08c7940fcad43ca1c94b
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-08 23:50:17 -0700

    Add ANDI instruction

---
+++ src/main/kotlin/simulator/impl/ANDI.kt
@@ -0,0 +1,24 @@
+package venus.simulator.impl
+
+import venus.riscv.Instruction
+import venus.riscv.InstructionField
+import venus.simulator.SimulatorState
+import venus.simulator.DispatchTest
+import venus.simulator.FieldTest
+import venus.simulator.InstructionImplementation
+
+object ANDI {
+    val implementation = object : InstructionImplementation {
+        override operator fun invoke(inst: Instruction, state: SimulatorState) {
+            val rs1: Int = inst.getField(InstructionField.RS1)
+            val imm: Int = signExtend(inst.getField(InstructionField.IMM_11_0), 12)
+            val rd: Int = inst.getField(InstructionField.RD)
+            state.setReg(rd, state.getReg(rs1) and imm)
+        }
+    }
+
+    val tests: List<DispatchTest> = listOf(
+        FieldTest(InstructionField.OPCODE, 0b0010011),
+        FieldTest(InstructionField.FUNCT3, 0b111)
+    )
+}
