6ff75b7198abb85fd054ac343e8d18f8a07c90ca
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-09 00:53:07 -0700

    Add Register name -> number conversion

---
+++ src/main/kotlin/assembler/RegisterNames.kt
@@ -0,0 +1,43 @@
+package venus.assembler
+
+fun regNameToNumber(reg: String): Int {
+    if (reg.startsWith("x")) {
+        val ret = reg.drop(1).toInt()
+        return if (ret in 0..31) ret else -1
+    }
+    return when (reg) {
+        "zero" -> 0
+        "ra" -> 1
+        "sp" -> 2
+        "gp" -> 3
+        "tp" -> 4
+        "t0" -> 5
+        "t1" -> 6
+        "t2" -> 7
+        "s0", "fp" -> 8
+        "s1" -> 9
+        "a0" -> 10
+        "a1" -> 11
+        "a2" -> 12
+        "a3" -> 13
+        "a4" -> 14
+        "a5" -> 15
+        "a6" -> 16
+        "a7" -> 17
+        "s2" -> 18
+        "s3" -> 19
+        "s4" -> 20
+        "s5" -> 21
+        "s6" -> 22
+        "s7" -> 23
+        "s8" -> 24
+        "s9" -> 25
+        "s10" -> 26
+        "s11" -> 27
+        "t3" -> 28
+        "t4" -> 29
+        "t5" -> 30
+        "t6" -> 31
+        else -> -1
+    }
+}
