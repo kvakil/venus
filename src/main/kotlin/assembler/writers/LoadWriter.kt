404aece38e8a2348c3750acbf1a0b2aea80c4c34
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-09 19:40:47 -0700

    Create LoadWriter
    This allows base displacement notation:
    
        lw $rd 0($rs1)

---
+++ src/main/kotlin/assembler/writers/LoadWriter.kt
@@ -0,0 +1,25 @@
+package venus.assembler.writers
+
+import venus.assembler.InstructionWriter
+import venus.assembler.Program
+import venus.riscv.Instruction
+import venus.riscv.InstructionField
+
+object LoadWriter : InstructionWriter() {
+    const val MAX_I_VALUE = 2047
+    const val MIN_I_VALUE = -2048
+
+    override operator fun invoke(prog: Program, inst: Instruction, args: List<String>) {
+        checkArgsLength(args, 3)
+
+        val rd = regNameToNumber(args[0])
+        val rs1 = regNameToNumber(args[2])
+
+        val imm = getImmediate(args[1], MIN_I_VALUE, MAX_I_VALUE)
+
+        inst.setField(InstructionField.RD, rd)
+        inst.setField(InstructionField.RS1, rs1)
+        inst.setField(InstructionField.IMM_11_0, imm)
+        prog.add(inst)
+    }
+}
