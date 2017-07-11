595b51d849f5693580f5de978649837c1ad90b9e
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-11 06:57:50 -0700

    Remove trailing spaces

--- src/main/kotlin/assembler/Linker.kt
+++ src/main/kotlin/assembler/Linker.kt
@@ -49,7 +49,7 @@ object Linker {
         val imm_11 = imm shr 20
         inst.setField(InstructionField.IMM_20, imm_20)
         inst.setField(InstructionField.IMM_10_1, imm_10_1)
-        inst.setField(InstructionField.IMM_19_12, imm_19_12)        
+        inst.setField(InstructionField.IMM_19_12, imm_19_12)
         inst.setField(InstructionField.IMM_11_J, imm_11)
     }
 }
