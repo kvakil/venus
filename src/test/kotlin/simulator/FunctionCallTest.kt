a43ca60796289d8e4691c392101852bf8f84c567
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-11 14:40:00 -0700

    Add jal and jalr test

---
+++ src/test/kotlin/simulator/FunctionCallTest.kt
@@ -0,0 +1,27 @@
+package venus.simulator
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import venus.riscv.Instruction
+import venus.assembler.Assembler
+import venus.assembler.Linker
+
+class FunctionCallTest {
+    @Test
+    fun doubleJALR() {
+        val prog = Assembler.assemble("""
+            jal x0 main
+        double:
+            add a0 a0 a0
+            jalr x0 ra 0
+        main:
+            addi a0 x0 5
+            jal ra double
+            add x1 a0 x0
+        """)
+       val linked = Linker.link(listOf(prog))
+       var sim = Simulator(linked.dump())
+       sim.run()
+       assertEquals(10, sim.state.getReg(1))
+    }
+}
