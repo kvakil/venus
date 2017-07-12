b81bd516531576153ea5afa1b37ad1b41ede088e
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-12 11:04:33 -0700

    Add nestedJALR test

--- src/test/kotlin/simulator/FunctionCallTest.kt
+++ src/test/kotlin/simulator/FunctionCallTest.kt
@@ -23,4 +23,30 @@ class FunctionCallTest {
         sim.run()
         assertEquals(10, sim.state.getReg(1))
     }
+
+    @Test
+    fun nestedJALR() {
+        val prog = Assembler.assemble("""
+            jal x0 main
+        foo:
+            addi s0 s0 1
+            jalr x0 ra 0
+        bar:
+            addi sp sp -4
+            sw 0(sp) ra
+            addi s0 s0 2
+            jal ra foo
+            lw ra 0(sp)
+            addi sp sp 4
+            jalr x0 ra 0
+        main:
+            addi s0 s0 4
+            addi sp sp 1000
+            jal ra bar
+        """)
+        val linked = Linker.link(listOf(prog))
+        val sim = Simulator(linked)
+        sim.run()
+        assertEquals(7, sim.state.getReg(8))
+    }
 }
