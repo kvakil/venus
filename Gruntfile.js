7dce2953e58c3506c32bd95ad2ca7e88463c2617
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-13 01:29:48 -0700

    Add uglify compressor

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,22 @@ module.exports = function(grunt) {
         qunit: {
             src: ['qunit/test.html'],
             force: true
+        },
+        uglify: {
+            options: {
+                banner: '/*! venus <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                compress: {
+                    unused: false
+                }
+            },
+            venus: {
+                files: {
+                    'venus.min.js': ['build/classes/main/**/*.js', 'build/classes/test/lib/kotlin-test*.js', 'build/classes/test/venus_test.js']
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.registerTask('test', 'qunit:src');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
