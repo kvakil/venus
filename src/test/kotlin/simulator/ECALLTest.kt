869204fcdb63700004f6088d83612e75fe0ccab4
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-13 21:35:24 -0700

    Add basic malloc test

--- src/test/kotlin/simulator/ECALLTest.kt
+++ src/test/kotlin/simulator/ECALLTest.kt
@@ -7,7 +7,7 @@ import venus.linker.Linker
 
 class ECALLTest {
     @Test
-    fun goBears() {
+    fun terminateEarly() {
         val prog = Assembler.assemble("""
         addi a0 x0 10
         ecall
@@ -16,5 +16,31 @@ class ECALLTest {
         val sim = Simulator(linked)
         sim.run()
         assertEquals(10, sim.state.getReg(10))
+    }
+
+    @Test
+    fun malloc() {
+        val prog = Assembler.assemble("""
+        # malloc 4 bytes of memory
+        addi a0 x0 9
+        addi a1 x0 4
+        ecall
+        mv s0 a0
+        # store 5 there
+        addi s1 x0 5
+        sw 0(s0) s1
+        # malloc 4 more bytes
+        addi a0 x0 9
+        addi a1 x0 4
+        ecall
+        addi s2 x0 6
+        # store 6 there
+        sw 0(a0) s2
+        # retrieve the old 5
+        lw s1 0(s0)""")
+        val linked = Linker.link(listOf(prog))
+        val sim = Simulator(linked)
+        sim.run()
+        assertEquals(5, sim.state.getReg(9))
     }
 }
