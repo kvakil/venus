e8b1db1773436f2cd2031bdd5f0339680d922916
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-14 12:59:20 -0700

    Add signed parts of RISC-M

---
+++ src/main/kotlin/simulator/impls/MULHImpl.kt
@@ -0,0 +1,16 @@
+package venus.simulator.impls
+
+import venus.riscv.Instruction
+import venus.riscv.InstructionField
+import venus.simulator.SimulatorState
+import venus.simulator.InstructionImplementation
+
+object MULHImpl : InstructionImplementation {
+    override operator fun invoke(inst: Instruction, state: SimulatorState) {
+        val rs1: Int = inst.getField(InstructionField.RS1)
+        val rs2: Int = inst.getField(InstructionField.RS2)
+        val rd: Int = inst.getField(InstructionField.RD)
+        state.pc += inst.length
+        TODO("implement MULHImpl")
+    }
+}
