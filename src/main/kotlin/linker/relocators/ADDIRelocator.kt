ae4eb94e810c3dc6a0b4c2fe7a670903d694123a
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-13 23:56:54 -0700

    Fix AUIPC/ADDI relocation

--- src/main/kotlin/linker/relocators/ADDIRelocator.kt
+++ src/main/kotlin/linker/relocators/ADDIRelocator.kt
@@ -7,6 +7,7 @@ import venus.linker.Relocator
 object ADDIRelocator : Relocator() {
     override operator fun invoke(inst: Instruction, pc: Int, target: Int) {
         val imm = target - (pc - 4)
-        inst.setField(InstructionField.IMM_11_0, imm)
+        var imm_lo = imm and 0b111111111111
+        inst.setField(InstructionField.IMM_11_0, imm_lo)
     }
 }
--- src/main/kotlin/linker/relocators/AUIPCRelocator.kt
+++ src/main/kotlin/linker/relocators/AUIPCRelocator.kt
@@ -6,7 +6,14 @@ import venus.linker.Relocator
 
 object AUIPCRelocator : Relocator() {
     override operator fun invoke(inst: Instruction, pc: Int, target: Int) {
-        val imm = (target - pc) shr 12
-        inst.setField(InstructionField.IMM_31_12, imm)
+        /* TODO: abstract this out, since both ADDIRelocator _and_ li need it */
+        val imm = target - pc
+        var imm_hi = imm ushr 12
+        var imm_lo = imm and 0b111111111111
+        if (imm_lo > 2047) {
+            imm_hi += 1
+            if (imm_hi == 1048576) imm_hi = 0
+        }
+        inst.setField(InstructionField.IMM_31_12, imm_hi)
     }
 }
