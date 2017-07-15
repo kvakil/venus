11857080feaad51e97b7ffbaceebe31553858f75
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-14 20:16:49 -0700

    Convert instructions to ITypeImpl

--- src/main/kotlin/simulator/impls/ORIImpl.kt
+++ src/main/kotlin/simulator/impls/ORIImpl.kt
@@ -1,16 +1,7 @@
 package venus.simulator.impls
 
-import venus.riscv.Instruction
-import venus.riscv.InstructionField
-import venus.simulator.SimulatorState
-import venus.simulator.InstructionImplementation
+import venus.simulator.impls.types.ITypeImpl
 
-object ORIImpl : InstructionImplementation {
-    override operator fun invoke(inst: Instruction, state: SimulatorState) {
-        val rs1: Int = inst.getField(InstructionField.RS1)
-        val imm: Int = signExtend(inst.getField(InstructionField.IMM_11_0), 12)
-        val rd: Int = inst.getField(InstructionField.RD)
-        state.setReg(rd, state.getReg(rs1) or imm)
-        state.pc += inst.length
-    }
+object ORIImpl : ITypeImpl() {
+    override fun evaluate(vrs1: Int, imm: Int): Int = vrs1 or imm
 }
--- src/main/kotlin/simulator/impls/SLTIImpl.kt
+++ src/main/kotlin/simulator/impls/SLTIImpl.kt
@@ -1,16 +1,7 @@
 package venus.simulator.impls
 
-import venus.riscv.Instruction
-import venus.riscv.InstructionField
-import venus.simulator.SimulatorState
-import venus.simulator.InstructionImplementation
+import venus.simulator.impls.types.ITypeImpl
 
-object SLTIImpl : InstructionImplementation {
-    override operator fun invoke(inst: Instruction, state: SimulatorState) {
-        val rs1: Int = inst.getField(InstructionField.RS1)
-        val imm: Int = signExtend(inst.getField(InstructionField.IMM_11_0), 12)
-        val rd: Int = inst.getField(InstructionField.RD)
-        state.setReg(rd, if (state.getReg(rs1) < imm) 1 else 0)
-        state.pc += inst.length
-    }
+object SLTIImpl : ITypeImpl() {
+    override fun evaluate(vrs1: Int, imm: Int): Int = if (vrs1 < imm) 1 else 0
 }
