49fdfdf3ac31c3c84c737e3916d19b7bb03b4ccd
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-15 11:39:50 -0700

    Remove unneeded peek

--- src/main/kotlin/simulator/History.kt
+++ src/main/kotlin/simulator/History.kt
@@ -4,7 +4,6 @@ class History {
     private val diffs = ArrayList<List<Diff>>()
 
     fun add(pre: List<Diff>) = diffs.add(pre.toList())
-    fun peek() = diffs.last()
     fun pop() = diffs.removeAt(diffs.size - 1)
     fun isEmpty() = diffs.isEmpty()
 }
