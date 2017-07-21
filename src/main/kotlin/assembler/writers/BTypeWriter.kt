edb25fec807891d79220f204ac5710006248caf6
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-21 16:37:47 -0700

    Add documentation to BTypeWriter

--- src/main/kotlin/assembler/writers/BTypeWriter.kt
+++ src/main/kotlin/assembler/writers/BTypeWriter.kt
@@ -6,10 +6,22 @@ import venus.assembler.AssemblerError
 import venus.riscv.Instruction
 import venus.riscv.InstructionField
 
+/**
+ * A singleton which can be called to write any BType instruction
+ */
 object BTypeWriter : InstructionWriter() {
     const val MAX_B_VALUE = 2047
     const val MIN_B_VALUE = -2048
 
+    /**
+     * Sets instruction fields and adds the instruction to the given program.
+     *
+     * @param prog the program to add the instruction to
+     * @param inst the instruction to fill in and add
+     * @param args the arguments given in the code
+     *
+     * @throws AssemblerError if the label doesn't exist, or is too far away
+     */
     override operator fun invoke(prog: Program, inst: Instruction, args: List<String>) {
         checkArgsLength(args, 3)
 
@@ -18,18 +30,16 @@ object BTypeWriter : InstructionWriter() {
 
         inst.setField(InstructionField.RS1, rs1)
         inst.setField(InstructionField.RS2, rs2)
-        val imm = prog.getLabelOffset(args[2])
-        if (imm == null) throw AssemblerError("could not find label ${args[2]}")
-        /* TODO: add branch bounds checking */
-        val imm_11 = imm shr 11
-        val imm_4_1 = imm shr 1
-        val imm_12 = imm shr 12
-        val imm_10_5 = imm shr 5
+        val label = args[2]
+        val imm = prog.getLabelOffset(label) ?:
+                throw AssemblerError("could not find label $label")
+        if (imm !in MIN_B_VALUE..MAX_B_VALUE)
+            throw AssemblerError("branch to $label too far")
 
-        inst.setField(InstructionField.IMM_11_B, imm_11)
-        inst.setField(InstructionField.IMM_4_1, imm_4_1)
-        inst.setField(InstructionField.IMM_12, imm_12)
-        inst.setField(InstructionField.IMM_10_5, imm_10_5)
+        inst.setField(InstructionField.IMM_11_B, imm shr 11)
+        inst.setField(InstructionField.IMM_4_1, imm shr 1)
+        inst.setField(InstructionField.IMM_12, imm shr 12)
+        inst.setField(InstructionField.IMM_10_5, imm shr 5)
 
         prog.add(inst)
     }
