7c7d8d544043f6381b62f56683502b610de6dc4f
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-22 21:47:30 -0700

    Document PseudoWriter

--- src/main/kotlin/venus/assembler/PseudoWriter.kt
+++ src/main/kotlin/venus/assembler/PseudoWriter.kt
@@ -2,7 +2,16 @@ package venus.assembler
 
 import venus.assembler.Assembler.AssemblerState
 
+/**
+ * Converts a pseudoinstruction into TAL instructions.
+ */
 abstract class PseudoWriter {
-    internal abstract operator fun invoke(args: LineTokens,
-        state: AssemblerState): List<LineTokens>
+    /**
+     * Transform a pseudoinstruction into TAL instructions.
+     *
+     * @param args the arguments originally given to the pseudoinstruction
+     * @param state the assembler's state
+     * @return a list of LineTokens corresponding to the TAL instruction
+     */
+    internal abstract operator fun invoke(args: LineTokens, state: AssemblerState): List<LineTokens>
 }
