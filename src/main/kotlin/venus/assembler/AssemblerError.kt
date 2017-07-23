a4e3b150a1b5b9042416e9bc8b844820fe091d15
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-22 19:28:47 -0700

    Document AssemblerError

--- src/main/kotlin/venus/assembler/AssemblerError.kt
+++ src/main/kotlin/venus/assembler/AssemblerError.kt
@@ -1,7 +1,19 @@
 package venus.assembler
 
+/**
+ * Thrown when errors occur during assembly.
+ *
+ * @todo split this into AssemblerUserError and AssemblerError
+ */
 class AssemblerError : Throwable {
+    /**
+     * @param msg the message to error with
+     */
     constructor(msg: String? = null) : super(msg)
 
+    /**
+     * @param line the line the error occurred on
+     * @param e the original error to pass along
+     */
     constructor(line: Int, e: Throwable) : super("${e.message} on line ${line}", e)
 }
