df836dedac1d372609d2d93ffc141198eba6fcef
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-27 17:07:03 -0700

    Add support for M extension

--- src/main/kotlin/venus/simulator/impls/DIVImpl.kt
+++ src/main/kotlin/venus/simulator/impls/DIVImpl.kt
@@ -3,5 +3,15 @@ package venus.simulator.impls
 import venus.simulator.impls.types.RTypeImpl
 
 object DIVImpl : RTypeImpl() {
-    override fun evaluate(vrs1: Int, vrs2: Int): Int = vrs1 / vrs2
+    override fun evaluate(vrs1: Int, vrs2: Int): Int {
+        if (vrs2 == 0) {
+            return -1
+        }
+
+        if (vrs1 == Int.MIN_VALUE && vrs2 == -1) {
+            return vrs1
+        }
+
+        return vrs1 / vrs2
+    }
 }
--- src/main/kotlin/venus/simulator/impls/MULHImpl.kt
+++ src/main/kotlin/venus/simulator/impls/MULHImpl.kt
@@ -3,5 +3,5 @@ package venus.simulator.impls
 import venus.simulator.impls.types.RTypeImpl
 
 object MULHImpl : RTypeImpl() {
-    override fun evaluate(vrs1: Int, vrs2: Int): Int = TODO("implement MULH")
+    override fun evaluate(vrs1: Int, vrs2: Int): Int = ((vrs1.toLong() * vrs2.toLong()) ushr 32).toInt()
 }
