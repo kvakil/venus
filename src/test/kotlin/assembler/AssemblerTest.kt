867d3a359a6c2733c728a37a56e0f40f3e326f9a
Keyhan Vakil <kvakil@berkeley.edu>
2017-07-31 12:12:59 -0700

    Replace var with val where applicable

--- src/test/kotlin/assembler/AssemblerTest.kt
+++ src/test/kotlin/assembler/AssemblerTest.kt
@@ -14,7 +14,7 @@ class AssemblerTest {
         add x3 x1 x2
         andi x3 x3 8
         """)
-        var sim = Simulator(Linker.link(listOf(prog)))
+        val sim = Simulator(Linker.link(listOf(prog)))
         sim.run()
         assertEquals(8, sim.getReg(3))
     }
@@ -26,7 +26,7 @@ class AssemblerTest {
         sw x1 60(x0)
         lw x2 -40(x1)
         """)
-        var sim = Simulator(Linker.link(listOf(prog)))
+        val sim = Simulator(Linker.link(listOf(prog)))
         sim.step()
         assertEquals(100, sim.getReg(1))
         sim.step()
@@ -45,7 +45,7 @@ class AssemblerTest {
         addi x9 x9 1
         bne x9 x6 start
         """)
-        var sim = Simulator(Linker.link(listOf(prog)))
+        val sim = Simulator(Linker.link(listOf(prog)))
         for (i in 1..17) sim.step()
         assertEquals(10, sim.getReg(8))
     }
