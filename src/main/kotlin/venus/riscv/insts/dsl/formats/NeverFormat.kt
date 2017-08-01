b8251aad3af8a56c4376473c317846273980c280
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-01 12:23:05 -0700

    Delete NeverFormat

--- src/main/kotlin/venus/riscv/insts/dsl/formats/NeverFormat.kt
+++
@@ -1,8 +0,0 @@
-package venus.riscv.insts.dsl.formats
-
-import venus.riscv.MachineCode
-
-class NeverFormat : InstructionFormat {
-    override fun matches(mcode: MachineCode): Boolean = false
-    override fun fill(): MachineCode = throw IllegalStateException("tried to fill a NeverFormat")
-}
