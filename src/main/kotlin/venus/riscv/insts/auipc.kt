f9d16c1f2ac8cb7c1ab7ea2e4645d9b0fc17a858
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-01 15:42:48 -0700

    Add UTypeInstruction

---
+++ src/main/kotlin/venus/riscv/insts/auipc.kt
@@ -0,0 +1,15 @@
+package venus.riscv.insts
+
+import venus.riscv.InstructionField
+import venus.riscv.insts.dsl.UTypeInstruction
+import venus.riscv.insts.dsl.impls.NoImplementation
+
+val auipc = UTypeInstruction(
+        name = "auipc",
+        opcode = 0b0100111,
+        impl32 = { mcode, sim ->
+            val offset = mcode[InstructionField.IMM_31_12] shl 12
+            sim.setReg(mcode[InstructionField.RD], sim.getPC() + offset)
+        },
+        impl64 = NoImplementation::invoke
+)
---
+++ src/main/kotlin/venus/riscv/insts/dsl/UTypeInstruction.kt
@@ -0,0 +1,28 @@
+package venus.riscv.insts.dsl
+
+import venus.riscv.MachineCode
+import venus.riscv.insts.dsl.formats.UTypeFormat
+import venus.riscv.insts.dsl.impls.RawImplementation
+import venus.riscv.insts.dsl.parsers.UTypeParser
+import venus.riscv.insts.dsl.relocators.InstructionRelocator32
+import venus.riscv.insts.dsl.relocators.InstructionRelocator64
+import venus.riscv.insts.dsl.relocators.NoRelocator32
+import venus.riscv.insts.dsl.relocators.NoRelocator64
+import venus.simulator.Simulator
+
+class UTypeInstruction(
+        name: String,
+        opcode: Int,
+        impl32: (MachineCode, Simulator) -> Unit,
+        impl64: (MachineCode, Simulator) -> Unit,
+        relocator32: InstructionRelocator32 = NoRelocator32,
+        relocator64: InstructionRelocator64 = NoRelocator64
+) : Instruction(
+        name = name,
+        format = UTypeFormat(opcode),
+        parser = UTypeParser,
+        impl32 = RawImplementation(impl32),
+        impl64 = RawImplementation(impl64),
+        relocator32 = relocator32,
+        relocator64 = relocator64
+)
