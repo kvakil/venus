b9672fa480adb1f0b28ab1c16affc07c4a5c4499
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-01 02:08:18 -0700

    Add ManualInstruction for special instructions

---
+++ src/main/kotlin/venus/riscv/insts/dsl/ManualInstruction.kt
@@ -0,0 +1,24 @@
+package venus.riscv.insts.dsl
+
+import venus.riscv.MachineCode
+import venus.riscv.Program
+import venus.simulator.Simulator
+
+class ManualInstruction (
+        name: String,
+        length: Int,
+        fieldPredicates: List<FieldEqual>,
+        private val eval32: (MachineCode, Simulator) -> Unit,
+        private val eval64: (MachineCode, Simulator) -> Unit = { _, _ -> TODO("no rv64 for $this") },
+        private val writer: (Program, MachineCode, List<String>) -> Unit
+) : Instruction(name, length) {
+    init {
+        ifields.addAll(fieldPredicates)
+    }
+
+    override fun impl32(mcode: MachineCode, sim: Simulator) { eval32(mcode, sim) }
+
+    override fun impl64(mcode: MachineCode, sim: Simulator) { eval64(mcode, sim) }
+
+    override fun write(prog: Program, mcode: MachineCode, args: List<String>) { writer(prog, mcode, args) }
+}
