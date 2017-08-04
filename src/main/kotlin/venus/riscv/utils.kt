843c144676637e043f5618b63870ca14219fffa0
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-04 14:44:13 -0700

    Add support for allowing character literals in immediates

--- src/main/kotlin/venus/riscv/utils.kt
+++ src/main/kotlin/venus/riscv/utils.kt
@@ -1,6 +1,9 @@
 package venus.riscv
 
 fun userStringToInt(s: String): Int {
+    if (isCharacterLiteral(s)) {
+        return s[1].toInt()
+    }
     val radix = when (s.take(2)) {
         "0x" -> 16
         "0b" -> 2
@@ -8,3 +11,6 @@ fun userStringToInt(s: String): Int {
     }
     return s.drop(2).toLong(radix).toInt()
 }
+
+private fun isCharacterLiteral(s: String) =
+        s.length == 3 && s[0] == '\'' && s[2] == '\''
