68b6239c96092e968da9960471b1d526131101ec
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-05 13:02:00 -0700

    Add missing sign extension for I types

--- src/main/kotlin/venus/riscv/insts/dsl/disasms/ITypeDisassembler.kt
+++ src/main/kotlin/venus/riscv/insts/dsl/disasms/ITypeDisassembler.kt
@@ -3,13 +3,14 @@ package venus.riscv.insts.dsl.disasms
 import venus.riscv.InstructionField
 import venus.riscv.MachineCode
 import venus.riscv.insts.dsl.Instruction
+import venus.riscv.insts.dsl.impls.signExtend
 
 object ITypeDisassembler : InstructionDisassembler {
     override fun invoke(mcode: MachineCode): String {
         val name = Instruction[mcode].name
         val rd = mcode[InstructionField.RD]
         val rs1 = mcode[InstructionField.RS1]
-        val imm = mcode[InstructionField.IMM_11_0]
+        val imm = signExtend(mcode[InstructionField.IMM_11_0], 12)
         return "$name x$rd x$rs1 $imm"
     }
 }
