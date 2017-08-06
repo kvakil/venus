63001b9ecd961b577ac8a18e1bf801e3754b8643
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-05 21:14:40 -0700

    Add shift-by-variable instructions

---
+++ src/main/kotlin/venus/riscv/insts/sra.kt
@@ -0,0 +1,12 @@
+package venus.riscv.insts
+
+import venus.riscv.insts.dsl.RTypeInstruction
+
+val sra = RTypeInstruction(
+        name = "sra",
+        opcode = 0b0110011,
+        funct3 = 0b101,
+        funct7 = 0b0100000,
+        eval32 = { a, b -> a shr b },
+        eval64 = { a, b -> a shr b.toInt() }
+)
---
+++ src/main/kotlin/venus/riscv/insts/srl.kt
@@ -0,0 +1,12 @@
+package venus.riscv.insts
+
+import venus.riscv.insts.dsl.RTypeInstruction
+
+val srl = RTypeInstruction(
+        name = "srl",
+        opcode = 0b0110011,
+        funct3 = 0b101,
+        funct7 = 0b0000000,
+        eval32 = { a, b -> a ushr b },
+        eval64 = { a, b -> a ushr b.toInt() }
+)
