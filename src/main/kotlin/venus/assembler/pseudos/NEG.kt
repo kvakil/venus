000d8587cc42a68f23a49c7a2b4caf907485622e
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-14 14:44:03 -0700

    Add NOT and NEG pseudoinstructions

---
+++ src/main/kotlin/venus/assembler/pseudos/NEG.kt
@@ -0,0 +1,13 @@
+package venus.assembler.pseudos
+
+import venus.assembler.AssemblerPassOne
+import venus.assembler.LineTokens
+import venus.assembler.PseudoWriter
+
+/** Writes pseudoinstruction `neg rd, rs` */
+object NEG : PseudoWriter() {
+    override operator fun invoke(args: LineTokens, state: AssemblerPassOne): List<LineTokens> {
+        checkArgsLength(args, 3)
+        return listOf(listOf("sub", args[1], "x0", args[2]))
+    }
+}
---
+++ src/main/kotlin/venus/assembler/pseudos/NOT.kt
@@ -0,0 +1,13 @@
+package venus.assembler.pseudos
+
+import venus.assembler.AssemblerPassOne
+import venus.assembler.LineTokens
+import venus.assembler.PseudoWriter
+
+/** Writes pseudoinstruction `not rd, rs` */
+object NOT : PseudoWriter() {
+    override operator fun invoke(args: LineTokens, state: AssemblerPassOne): List<LineTokens> {
+        checkArgsLength(args, 3)
+        return listOf(listOf("xori", args[1], args[2], "-1"))
+    }
+}
