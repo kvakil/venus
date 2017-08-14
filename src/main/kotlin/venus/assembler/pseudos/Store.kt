20ceeede4401750a9e76b8ad102644433e74814e
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-14 15:29:45 -0700

    Fix bug where store immediate was treated as label

--- src/main/kotlin/venus/assembler/pseudos/Store.kt
+++ src/main/kotlin/venus/assembler/pseudos/Store.kt
@@ -5,6 +5,7 @@ import venus.assembler.LineTokens
 import venus.assembler.PseudoWriter
 import venus.riscv.insts.dsl.relocators.PCRelHiRelocator
 import venus.riscv.insts.dsl.relocators.PCRelLoRelocator
+import venus.riscv.userStringToInt
 
 /**
  * Writes a store pseudoinstruction. (Those applied to a label)
@@ -12,12 +13,20 @@ import venus.riscv.insts.dsl.relocators.PCRelLoRelocator
 object Store : PseudoWriter() {
     override operator fun invoke(args: LineTokens, state: AssemblerPassOne): List<LineTokens> {
         checkArgsLength(args, 4)
+        val label = args[2]
+        try {
+            userStringToInt(label)
+            /* if it's a number, this is just an ordinary store instruction */
+            return listOf(args)
+        } catch (e: NumberFormatException) {
+            /* assume it's a label */
+        }
 
         val auipc = listOf("auipc", args[3], "0")
-        state.addRelocation(PCRelHiRelocator, state.getOffset(), args[2])
+        state.addRelocation(PCRelHiRelocator, state.getOffset(), label)
 
         val store = listOf(args[0], args[1], "0", args[3])
-        state.addRelocation(PCRelLoRelocator, state.getOffset() + 4, args[2])
+        state.addRelocation(PCRelLoRelocator, state.getOffset() + 4, label)
 
         return listOf(auipc, store)
     }
