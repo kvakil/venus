58ff631144f1c7d73a1e752e367d842453d34f0f
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-15 19:47:15 -0700

    Fix to allow negative signs for hex/binary immediates

--- src/main/kotlin/venus/riscv/utils.kt
+++ src/main/kotlin/venus/riscv/utils.kt
@@ -4,12 +4,22 @@ fun userStringToInt(s: String): Int {
     if (isCharacterLiteral(s)) {
         return characterLiteralToInt(s)
     }
-    val radix = when (s.take(2)) {
-        "0x" -> 16
-        "0b" -> 2
+
+    val radix = when {
+        s.startsWith("0x") -> 16
+        s.startsWith("0b") -> 2
+        s.drop(1).startsWith("0x") -> 16
+        s.drop(1).startsWith("0b") -> 2
         else -> return s.toLong().toInt()
     }
-    return s.drop(2).toLong(radix).toInt()
+
+    val skipSign = when (s.first()) {
+        '+', '-' -> 1
+        else -> 0
+    }
+
+    val noRadixString = s.take(skipSign) + s.drop(skipSign + 2)
+    return noRadixString.toLong(radix).toInt()
 }
 
 private fun isCharacterLiteral(s: String) =
