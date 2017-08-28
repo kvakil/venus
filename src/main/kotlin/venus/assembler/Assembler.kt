7f41809de78f682a829dbbaf2a15ae8b9e8b2401
Keyhan Vakil <kvakil@berkeley.edu>
2017-08-28 15:16:21 -0700

    Change to more idiomatic ".lines()"

--- src/main/kotlin/venus/assembler/Assembler.kt
+++ src/main/kotlin/venus/assembler/Assembler.kt
@@ -66,7 +66,7 @@ internal class AssemblerPassOne(private val text: String) {
     }
 
     private fun doPassOne() {
-        for (line in text.split('\n')) {
+        for (line in text.lines()) {
             try {
                 currentLineNumber++
 
